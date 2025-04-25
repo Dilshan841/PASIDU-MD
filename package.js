a619cdcaf2417dd0c9de20c77da5659ef77ad425
Dilshan841 <rathnayakehemantha427@gmail.com>
2025-04-25 07:15:12 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+  "name": "dilshan-md-bot",
+  "version": "1.0.0",
+  "description": "WhatsApp Multi-Device Bot by Dilshan Ashinsa",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "pair": "node pair.js"
+  },
+  "keywords": [
+    "whatsapp-bot",
+    "multi-device",
+    "baileys",
+    "dilshan-md"
+  ],
+  "author": "Dilshan Ashinsa",
+  "license": "MIT",
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.6.0",
+    "@hapi/boom": "^10.0.0",
+    "dotenv": "^16.4.5",
+    "chalk": "^5.3.0",
+    "figlet": "^1.6.0"
+  }
+}
